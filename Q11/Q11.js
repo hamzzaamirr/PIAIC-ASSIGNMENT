a5e8c1804004b00cf390e5c0e2e399dcfc7a71ee
Hamza Amir <hamza27397@gmail.com>
2023-03-25 04:32:50 +0500

    Question 11

---
+++ Q11/Q11.js
@@ -0,0 +1,8 @@
+/*
+Names: Store the names of a few of your friends in a array called names. Print each person’s name
+by accessing each element in the list, one at a time.
+*/
+var myarr = ["Hamza", "Amir", "Asad", "Ali"];
+for (var i = 0; i < myarr.length; i++) {
+    console.log(myarr[i]);
+}
