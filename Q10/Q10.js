99330a1a58055e8bbad2cdf54c43e0ef40bf842b
Hamza Amir <hamza27397@gmail.com>
2023-03-25 03:38:28 +0500

    QUESTION 10

---
+++ Q10/Q10.js
@@ -0,0 +1,10 @@
+/*10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to
+each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top
+of each program file. Then write one sentence
+describing what the program does.
+*/
+// Name: Hamza Amir => Date: 25-03-23
+// This code is converting Upper case string into Lower case string 
+var mystring = "HAMZA";
+console.log(mystring.toLowerCase());
