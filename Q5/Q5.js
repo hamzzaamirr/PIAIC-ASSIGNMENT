23cac5f5078ea87d61246a515cdc59c261b909ed
Hamza Amir <hamza27397@gmail.com>
2023-03-25 03:06:14 +0500

    Q5 and Q6 update

--- Q5/Q5.js
+++ Q5/Q5.js
@@ -1,6 +1,3 @@
 var famous_person = "John Cena";
 var message = "Never Give Up";
 console.log("".concat(famous_person, " Once said, \"").concat(message, "\""));
-var myname = "\t\tHamza\nAmir";
-console.log(myname);
-console.log(myname.trim());
--- Q5/Q5.ts
+++ Q5/Q5.ts
@@ -1,6 +1,3 @@
 let famous_person:string = "John Cena";
 let message:string = "Never Give Up";
 console.log(`${famous_person} Once said, "${message}"`);
-let myname:string = "\t\tHamza\nAmir";
-console.log(myname);
-console.log(myname.trim());
